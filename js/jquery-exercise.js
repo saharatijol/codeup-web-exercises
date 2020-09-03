7fb921e51857002c10561f84f23dd0ab14a9b294
Sara T <sahara.tijol@gmail.com>
2020-09-03 09:22:44 -0500

    morning push ups

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -1,5 +1,18 @@
 "use strict";
 
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
-});
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+//
+// var contents = $('#top').html();
+// alert(contents);
+//
+// var border = $('.codeup').css('border', '2px solid #000');
+
+
+$('li').css('font-size', '20px');
+
+$('h1, p, li').css('background-color', '#FFFF00')
+
+var contents = $('h1').html();
+alert(contents);
---
+++ morning-pushups/array-avg.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function avgArr(arr) {
+    let sumArr = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sumArr += arr[i];
+    }
+    return sumArr/arr.length;
+}
+
+var arr = [4,5,6,7,9,6,7]
+
+console.log(sumArr[4,5,6,7,10]);
