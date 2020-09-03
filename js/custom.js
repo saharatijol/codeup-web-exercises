9455a6804e2c00000412c23a02ac7e7d86c6e0a5
Sara T <sahara.tijol@gmail.com>
2020-09-03 06:51:25 -0500

    jquery test

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// $(document).ready(function() {
+//     alert("The DOM has finished loading!");
+// });
+
+// ID Selector
+// var contents = $('#codeup').html();
+// alert(contents);
+
+// Class Selector
+$('.important').css('background-color', 'yellow');
+
+// Element Selector
+$('p').css('font-size', '14px');
+
+// Multiple Selectors
