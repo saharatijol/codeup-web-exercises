8788d59695f932d6c7dabb6186cb5ef98277c9b9
Sara T <sahara.tijol@gmail.com>
2020-09-07 16:48:26 -0500

    Refactored loops exercise1

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,61 +1,100 @@
 'use strict';
 
-/////// returnMultiplicationTable Function ////////
-function returnMultiplicationTable(input) { //i don't understand. Help
-    let output = ''
+// TODO: Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+function showMultiplicationTable(num) {
     for (let i = 1; i < 11; i++) {
-        output += input + "" + " x " + i + " = " + (input*i) // shorthand output +=
-        if (i !== 10) {
-            output += '\n' //shorthand output +=
+        let multiply = num * i;
+        console.log(`${num} X ${i} = ${multiply}`);
+    }
+}
+
+// TODO: Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even.
+// * Better Version: Divide the work by creating function
+function randomNumber() {
+    return Math.floor(Math.random() * (200 - 20 + 1)) + 20;
+}
+
+function isNumberEven(num) {
+    var result = 'is odd';
+    if (num % 2 === 0) {
+        result = 'is even';
+    }
+    return result;
+}
+
+function oddEvenMessageV2() {
+    console.time('oddEvenMessageV2');
+    for (let i = 0; i < 10; i++) {
+        let random = randomNumber();
+        console.log(`${random} ${isNumberEven(random)}`);
+    }
+    console.timeEnd('oddEvenMessageV2'); //* Checks speed of execution in ms
+}
+//oddEvenMessageV2();
+
+// * Shorter Version
+function oddEvenMessageV1() {
+    console.time('oddEvenMessageV1');
+    for (let i = 0; i < 10; i++) {
+        let random = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
+        if (random % 2 === 0) {
+            console.log(`${random} is even`);
+        } else {
+            console.log(`${random} is odd`);
         }
     }
-    return output;
+    console.timeEnd('oddEvenMessageV1'); // * check speed of execution in ms
 }
-// (returnMultiplicationTable(7)
-// console.log(returnMultiplicationTable(7))
+//oddEvenMessageV1();
 
-//////// returnEvenOddMessage Function //////
-function returnEvenOddMessage(num) {
-    if (num % 2 === 0) {
-        return String(num) + " is even";
-    }
-    else {
-        return String(num) + " is odd";
-    }
-}
-
-
-///////// returnEvenOddMessages Function/////////
-function returnTenEvenOddMessages() { //how to return and get it to pass tests?
-    for(let i = 1; i < 11 ; i++) {
-       var randomNum = Math.floor(Math.random() * 180) + 20;
-       if (randomNum % 2 === 0) {
-           console.log(randomNum + " is " +  " even");
-       }
-       else {
-           console.log(randomNum + " is " + " odd");
-       }
-   }
-}
-console.log(returnTenEvenOddMessages())
-
-
-/////////// returnNumberSail Function //////////
-function returnNumberSail(stringNum) { // what happened?
+// TODO: Create a for loop that uses console.log to create the output shown below.
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+// 88888888
+// 999999999
+function numberSail() {
     for (let i = 1; i < 10; i++) {
         console.log(("" + i).repeat(i));
     }
 }
-console.log(returnNumberSail);
+//numberSail();
 
-
-////////// returnCountDownFrom100InFives Function ////////
-function returnCountDownFrom100InFives() { // how to return instead of console.log
-    for(let i = 100; i > 4 ; i-=5) {
+// TODO: Create a for loop that uses console.log to create the output shown below.
+// 100
+// 95
+// 90
+// 85
+// 80
+// 75
+// 70
+// 65
+// 60
+// 55
+// 50
+// 45
+// 40
+// 35
+// 30
+// 25
+// 20
+// 15
+// 10
+// 5
+function countDown() {
+    for (let i = 100; i > 0; i-=5) {
         console.log(i);
     }
 }
-//console.log(returnCountDownFrom100InFives());
+
+//countDown();
+
+
+
 
 
 
